e85cdbc9a094fcfeee8377bda0b2401d0055c841
JohnVSCordeiro <joao.cordeiro@sptech.school>
2023-03-22 10:48:06 -0300

    @JohnVSCordeiro subindo projeto com atualizações na estilização

--- Node/index.js
+++ Node/index.js
@@ -10,13 +10,15 @@ const Post = require('./models/Post');
 
 
 // config
-    // template engine
-    app.engine('handlebars', handlebars.engine({defaultLayout: 'main',
-        runtimeOptions: {
+// template engine
+app.engine('handlebars', handlebars.engine({
+    defaultLayout: 'main',
+    runtimeOptions: {
         allowProtoPropertiesByDefault: true,
-        allowProtoMethodsByDefault: true,},
-    }))
-    app.set('view ensine', 'handlebars')
+        allowProtoMethodsByDefault: true,
+    },
+}))
+app.set('view ensine', 'handlebars')
 
 
 // Conexão com o banco de dados MySQL (Conectar com banco de dados)
@@ -28,57 +30,68 @@ const Post = require('./models/Post');
 //});
 
 // body Parser confisuração (PEgar dado de um formulario)
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 app.use(bodyParser.json())
 
 
 // Rotas
 
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
     res.render('C:/Users/joaoj/OneDrive/Área de Trabalho/Node/views/layouts/home.handlebars')
 })
-    
 
 
 
-app.get('/cad', function(req,res){
-        res.render('C:/Users/joaoj/OneDrive/Área de Trabalho/Node/views/layouts/formulario.handlebars')
-    });
 
-    app.get('/lista', function(req,res){
-        Post.findAll({order: [['id', 'DESC']]}).then(function(post){ 
-            res.render('C:/Users/joaoj/OneDrive/Área de Trabalho/Node/views/layouts/lista.handlebars', {posts : post} )
+app.get('/cad', function (req, res) {
+    res.render('C:/Users/joaoj/OneDrive/Área de Trabalho/Node/views/layouts/formulario.handlebars')
+});
 
+app.get('/lista', function (req, res) {
+    Post.findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then(function (post) {
+        res.render('C:/Users/joaoj/OneDrive/Área de Trabalho/Node/views/layouts/lista.handlebars', {
+            posts: post
         })
-       
+
     })
 
+})
 
-    app.post('/add', function(req,res){
-       Post.create({
+
+app.post('/add', function (req, res) {
+    Post.create({
         titulo: req.body.titulo,
         conteudo: req.body.conteudo
-       }).then(function(){
+    }).then(function () {
         res.redirect("/")
-       }).catch(function(){
+    }).catch(function () {
         res.send("ERRO ao criar o formulario" + erro)
-       })
+    })
 
-    });
+});
 
-    app.get('/deletar/:id', function(req,res){
-        Post.destroy({where: {'id' : req.params.id}}).then(function(){
+app.get('/deletar/:id', function (req, res) {
+    Post.destroy({
+        where: {
+            'id': req.params.id
+        }
+    }).then(function () {
         res.send("Postagem deletada com sucesso")
-        }).catch(function(erro){
-          res.send('Essa postagem não existe')}) 
-    }) 
+    }).catch(function (erro) {
+        res.send('Essa postagem não existe')
+    })
+})
 
 
 
 
-app.listen(9922, function(){
+app.listen(9922, function () {
     console.log("Servidor rodando na url http://localhost:9922/");
 
 });
-   
-
